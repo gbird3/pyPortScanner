36f0498d45ec2bbb9f5f4971d4cbe7344a989795
Gregory Bird <gregbird12@gmail.com>
2017-02-23 20:51:59 -0700

    added ability to scan multiple ports. Added comments to the code

--- scan.py
+++ scan.py
@@ -6,6 +6,12 @@ import subprocess
 import itertools
 
 # Get user input through the use of command line switches
+# Allows the user to specify a single IP address
+# i.e. -i 192.168.1.7
+# or an IP range
+# i.e. -i 192.168.1.1-254
+# Also allows the user to specify one or multiple ports
+# -p 80 or -p '22, 80, 443'
 def userInput(argv):
    host = ''
    port = ''
@@ -22,20 +28,21 @@ def userInput(argv):
        elif opt in ("-i", "--ip"):
            host = arg
        elif opt in ("-p", "--port"):
-            port = arg
+           port = arg.replace(" ", "")
 
    info = {
        'host': host,
        'port': port
    }
 
-   print('Starting port scan on {} for port {}'.format(host, port))
+   print('Starting port scan on {} for port(s) {}'.format(host, port))
 
    return info
 
+# Check if the argument is an ipRange. If so, get the list of IP addresses and then call the scanPorts Function
+# If not, call the scan Ports function on the solo IP
 # based off code found http://stackoverflow.com/questions/20525330/python-generate-a-list-of-ip-addresses-from-user-input?answertab=votes#answer-20525570
 def ip_range(ipRange, port):
-
     if "-" in ipRange:
         octets = ipRange.split('.')
 
@@ -44,16 +51,29 @@ def ip_range(ipRange, port):
 
         for address in itertools.product(*ranges):
             ip = '.'.join(map(str, address))
-            scanPorts(ip, port)
+            print()
+            print('Scanning {}'.format(ip))
+            getPorts(ip, port)
 
     else:
         ip = ipRange
-        scanPorts(ip, port)
+        getPorts(ip, port)
 
-# Function to scan the hot and ip address
-def scanPorts(ip, port):
+# Checks if multiple ports were specified and calls the ScanPort function for each port
+def getPorts(ip, port):
+    if "," in port:
+        ports = port.split(',')
+
+        for port in ports:
+            scanPort(ip, port)
+    else:
+        scanPort(ip, port)
+
+# Function to scan the host and ports using socket
+def scanPort(ip, port):
+    iport = int(port)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    result = s.connect_ex((ip, int(port)))
+    result = s.connect_ex((ip, iport))
 
     if result == 0:
         print("Port {}: Open".format(port))
